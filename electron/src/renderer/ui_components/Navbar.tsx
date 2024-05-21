5bbabc85bc2b4786632e4b2565f9f1ead274c159
Jamie Cole <jamiejcole@icloud.com>
2024-05-21 21:25:53 +1000

    Chore: added md and lg support for navbar

--- electron/src/renderer/ui_components/Navbar.tsx
+++ electron/src/renderer/ui_components/Navbar.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import React, { useState } from 'react';
+import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import {
     ArrowTrendingUpIcon,
@@ -11,7 +11,6 @@ import {
 } from '@heroicons/react/24/solid';
 
 const Navbar = () => {
-    const [mobileMenuShown, setMobileMenuShown] = useState<boolean>(false);
     const locationPathName = useLocation().pathname;
 
     const links = (
@@ -19,7 +18,6 @@ const Navbar = () => {
         linkStyle: string,
         iconStyle: string,
         textStyle: string,
-        mobile: boolean,
     ) => {
         const linkList: {
             name: string;
@@ -52,13 +50,12 @@ const Navbar = () => {
             <div className={divStyle}>
                 {linkList.map((value, index) => {
                     let fullLinkStyle = linkStyle;
-                    if (!mobile) {
-                        fullLinkStyle += ` rounded-lg ${
-                            locationPathName === value.link
-                                ? 'bg-red-500 shadow-sm'
-                                : 'hover:bg-gray-100 bg-transparent'
-                        }`;
-                    }
+                    fullLinkStyle += ` rounded-lg ${
+                        locationPathName === value.link
+                            ? 'bg-red-500 shadow-sm'
+                            : 'hover:bg-gray-100 bg-transparent'
+                    }`;
+
                     return (
                         <Link
                             to={value.link}
@@ -83,13 +80,7 @@ const Navbar = () => {
                     NSBL
                 </h1>
             </div>
-            {links(
-                'flex flex-col gap-8',
-                'flex gap-3 p-2',
-                'w-6 h-6',
-                '',
-                false,
-            )}
+            {links('flex flex-col gap-8', 'flex gap-3 p-2', 'w-6 h-6', '')}
             <button
                 type="button"
                 // onClick={() => {
@@ -114,7 +105,6 @@ const Navbar = () => {
                 'w-20 h-16 flex flex-col gap-1 justify-center items-center',
                 'w-6 h-6',
                 '',
-                false,
             )}
             <button
                 type="button"
@@ -132,66 +122,12 @@ const Navbar = () => {
         </div>
     );
 
-    const small = () => (
-        <div
-            className={`md:-right-1/2 absolute duration-300 right-0 bottom-0 h-full w-1/2 bg-gray-100 flex flex-col justify-around items-center p-5 ${
-                mobileMenuShown ? 'flex' : '-right-1/2'
-            }`}
-        >
-            <h1 className="text-2xl font-bold">Title</h1>
-            {links('flex flex-col gap-10', 'flex gap-3', 'w-6 h-6', '', true)}
-            <button
-                type="button"
-                onClick={() => {
-                    // navigate(`/profile`);
-                }}
-                className="font-light flex gap-3"
-            >
-                <Cog6ToothIcon
-                    className="w-6 h-6 "
-                    style={{ strokeWidth: 1 }}
-                />
-                Settings
-            </button>
-        </div>
-    );
-
     return (
         <nav className="overflow-hidden w-fit flex-none">
             {/* styles for screens greater than lg */}
             {large()}
             {/* styles for screens greater than md */}
             {medium()}
-            {/* styles for screens smaller than md */}
-            {small()}
-
-            {/* Mobile Hambuger Menu */}
-            <div
-                className="absolute right-5 bottom-5 flex md:hidden bg-blue-500 flex-col gap-1 p-4 rounded-full"
-                onClick={() => setMobileMenuShown(!mobileMenuShown)}
-            >
-                <div className="h-0.5 sm:flex hidden" />
-                <div
-                    className={`block bg-white w-4 duration-300 h-0.5 sm:w-8 sm:h-1 rounded-2xl ${
-                        mobileMenuShown
-                            ? 'translate-y-1.5 sm:translate-y-2 -rotate-45'
-                            : ''
-                    }`}
-                />
-                <div
-                    className={`block bg-white w-4 duration-300 h-0.5 sm:w-8 sm:h-1 rounded-2xl ${
-                        mobileMenuShown ? 'opacity-0' : ''
-                    }`}
-                />
-                <div
-                    className={`block bg-white w-4 duration-300 h-0.5 sm:w-8 sm:h-1 rounded-2xl ${
-                        mobileMenuShown
-                            ? '-translate-y-1.5 sm:-translate-y-2 rotate-45'
-                            : ''
-                    }`}
-                />
-                <div className="h-0.5 sm:flex hidden" />
-            </div>
         </nav>
     );
 };
