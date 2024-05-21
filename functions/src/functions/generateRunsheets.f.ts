f847dde83d9d3fc52921e1aab67e1c0a6ff301e6
tobsterclark <me@tobyclark.dev>
2024-05-21 20:47:44 +1000

    PDFs are now being generated from the DB

--- functions/src/functions/generateRunsheets.f.ts
+++ functions/src/functions/generateRunsheets.f.ts
@@ -1,58 +1,82 @@
 import { onCall, onRequest } from "firebase-functions/v2/https";
 import { promises as fs } from "fs";
 import { PDFDocument, TextAlignment } from "pdf-lib";
-import prismaClient from "@tobsterclark/prisma-orm";
+import prismaClient, { Game, Prisma } from "@tobsterclark/prisma-orm";
+
+const db = new prismaClient();
+type ScoresheetResult = Prisma.GameGetPayload<{
+  include: {
+    team_dark: { include: { players: true } };
+    team_light: { include: { players: true } };
+    timeslot: true;
+  };
+}>;
 
 export default onRequest(
   { region: "australia-southeast1", labels: { test: "test" } },
   async (req, res) => {
-    console.log("function running");
-
-    const client = new prismaClient();
-
     // Get Data
-    // prismaClient.game
+    const games: ScoresheetResult[] = await db.game.findMany({
+      take: 5,
+      include: {
+        team_dark: { include: { players: true } },
+        team_light: { include: { players: true } },
+        timeslot: true,
+      },
+    });
 
-    // Load PDF
-    // const pdfTemplateFs = await fs.readFile("src/resources/scoresheet_template.pdf");
-    // const pdfTemplate = await PDFDocument.load(pdfTemplateFs);
+    for (const game of games) {
+      await createPdf(game);
+    }
 
-    // for (let val of test.items) {
-    // 	console.log(val.data);
-    // 	if (!val.data) {
-    // 		console.warn("no data");
-    // 		continue;
-    // 	}o
-
-    // 	const gameData = {
-    // 		black_team: val.data["whiteTeam"]["title"],
-    // 		white_team: val.data["opponentTeam"]["title"],
-    // 		court: val.data["court"] + " ",
-    // 		location: val.data["location"],
-    // 	};
-
-    // 	const pdfDoc = await pdfTemplate.copy();
-
-    // 	const form = pdfDoc.getForm();
-    // 	const fields = form.getFields();
-
-    // 	form.getTextField("Team A").setText("the team");
-    // 	form.getTextField("Team B").setText("random team");
-    // 	form.getTextField("Venue").setText("test venue");
-    // 	form.getTextField("Court").setText("2");
-    // 	form.getTextField("Date").setText("14th March");
-    // 	form.getTextField("Time").setText("4:00pm");
-    // 	form.getTextField("white_num_1").setText("23");
-    // 	form.getTextField("white_player_1").setText("john");
-
-    // 	form.flatten();
-
-    // 	const bytes = await pdfDoc.save();
-    // 	await fs.writeFile("src/resources/out/test.pdf", bytes);
-    // }
-
-    // .res
-    //   .status(200)
-    //   .send();
+    res.status(200).send();
   },
 );
+
+const createPdf = async (game: ScoresheetResult) => {
+  // Load PDF
+  const pdfTemplateFs = await fs.readFile(
+    __dirname + "/../resources/scoresheet_template.pdf",
+  );
+
+  const pdfTemplate = await PDFDocument.load(pdfTemplateFs);
+  const form = pdfTemplate.getForm();
+
+  const gameTime = game.timeslot.date.toLocaleTimeString("en-au", {
+    hour12: true,
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+
+  // Fill in general info
+  form.getTextField("Team A").setText(game.team_light.name);
+  form.getTextField("Team B").setText(game.team_dark.name);
+  form.getTextField("Venue").setText(game.timeslot.location);
+  form.getTextField("Court").setText(game.timeslot.court.toString());
+  form.getTextField("Date").setText("14th March");
+  form.getTextField("Time").setText(gameTime);
+
+  // Fill in light players
+  game.team_light.players.forEach((player, index) => {
+    form.getTextField(`white_player_${index + 1}`).setText(player.first_name);
+    form
+      .getTextField(`white_num_${index + 1}`)
+      .setText(player.number?.toString());
+  });
+
+  // Fill in dark players
+  game.team_dark.players.forEach((player, index) => {
+    form.getTextField(`black_player_${index + 1}`).setText(player.first_name);
+    form
+      .getTextField(`black_num_${index + 1}`)
+      .setText(player.number?.toString());
+  });
+
+  form.flatten();
+
+  const bytes = await pdfTemplate.save();
+  await fs.writeFile(
+    __dirname + `/../resources/out/test+${game.id}.pdf`,
+    bytes,
+  );
+};
