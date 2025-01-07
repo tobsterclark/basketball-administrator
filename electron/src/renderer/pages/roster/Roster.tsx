4702a356b5ff6b143b47fe5e6afcbefcdca4e5e4
Jamie Cole <jamiejcole@icloud.com>
2025-01-07 20:49:44 +1100

    feat: events in roster, corresponding bg color, sorted, appropriate labels/headers

--- electron/src/renderer/pages/roster/Roster.tsx
+++ electron/src/renderer/pages/roster/Roster.tsx
@@ -7,6 +7,7 @@ import {
     AppointmentTooltip,
     DateNavigator,
     Toolbar,
+    Resources,
 } from '@devexpress/dx-react-scheduler-material-ui';
 import { TodayButton, ViewState } from '@devexpress/dx-react-scheduler';
 import PageContainer from '../../ui_components/PageContainer';
@@ -18,11 +19,27 @@ import {
 } from '../../../general/prismaTypes';
 import { IpcChannels } from '../../../general/IpcChannels';
 import Terms2025 from '../data/Terms';
+import React from 'react';
 
 enum Location {
     ST_IVES = 'ST_IVES',
     BELROSE = 'BELROSE',
 }
+
+const locationToText = (location: Location) => {
+    switch (location) {
+        case Location.ST_IVES:
+            return 'St Ives';
+        case Location.BELROSE:
+            return 'Belrose';
+    }
+}
+
+const formatTime = (date: Date) => {
+    // format time in <HH:mm> AM/PM format
+    return date.toLocaleTimeString([], { hour: 'numeric', minute: '2-digit' });
+}
+    
 
 type Team = {
     id: string;
@@ -57,13 +74,46 @@ type Event = {
     endDate: Date;
     id: string;
     location: string;
+    court: number;
 };
 
-const CustomAppointment = styled(Appointments.Appointment)(({ theme }) => ({
-    backgroundColor: '#000000',
-    borderRadius: '8px',
-    color: '#fff',
-}));
+const courtData = [
+    {
+        text: "Court 1",
+        id: 1,
+        color: "#cb6bb2"
+    },
+    {
+        text: "Court 2",
+        id: 2,
+        color: "#56ca85"
+    },
+    {
+        text: "Court 3",
+        id: 3,
+        color: "#1e90ff"
+    }
+];
+
+const testResources = [
+    {
+        fieldName: "court",
+        title: "court",
+        instances: courtData
+    }
+];
+
+const CustomAppointment = ({ children, data, ...restProps }: { children: React.ReactNode, data: any }) => (
+    <Appointments.Appointment {...restProps} className="!bg-transparent !cursor-default" data={data} onClick={() => {}} draggable={false} resources={[]}>
+        <div className={`text-white px-1 py-1 ${data.location === Location.ST_IVES ? 'bg-blue-500' : 'bg-green-500'}`}>
+            <div className='font-bold'>{data.title}</div>
+            <div>{formatTime(data.startDate)} - {formatTime(data.endDate)}</div>
+            <div>{locationToText(data.location)}</div>
+            <div>Court {data.court}</div>
+        </div>
+    </Appointments.Appointment>
+);
+
 
 const Roster = () => {
     const [allGames, setAllGames] = useState<Game[]>([]);
@@ -74,19 +124,27 @@ const Roster = () => {
             const startDate = new Date(game.timeslot.date);
             const endDate = new Date(startDate);
             endDate.setHours(startDate.getHours() + 1);
-
+    
             return {
                 title: `${game.lightTeam.name} vs ${game.darkTeam.name}`,
                 startDate,
                 endDate,
                 id: game.id,
                 location: game.timeslot.location,
+                court: game.timeslot.court,
             };
         });
-
+    
+        // Sort events by location first (so st ives is first, belrose is second), then sort by court number
+        events.sort((a, b) => {
+            if (a.location < b.location) return 1;
+            if (a.location > b.location) return -1;
+            if (a.court < b.court) return -1;
+            if (a.court > b.court) return 1;
+            return 0;
+        });
+    
         setAllEvents(events);
-        console.log('events:');
-        console.log(events);
     };
 
     useEffect(() => {
@@ -138,12 +196,14 @@ const Roster = () => {
             </Button>
             <div className="pt-4">{/* <Scheduler data={allEvents} /> */}</div>
             <Scheduler data={allEvents}>
+                
                 <ViewState defaultCurrentDate={new Date()} />
                 <DayView startDayHour={8} endDayHour={16} />
                 <Toolbar />
                 <DateNavigator />
-                <Appointments appointmentComponent={CustomAppointment} />
+                <Appointments appointmentComponent={CustomAppointment}/>
                 <AppointmentTooltip />
+                <Resources data={testResources} mainResourceName='court' />
             </Scheduler>
         </PageContainer>
     );
