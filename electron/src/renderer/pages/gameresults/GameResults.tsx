622b0912139a2a4e69ddc593b2a28eb55e8895d2
Jamie Cole <jamiejcole@icloud.com>
2025-01-13 11:33:45 +1100

    k

--- electron/src/renderer/pages/gameresults/GameResults.tsx
+++ electron/src/renderer/pages/gameresults/GameResults.tsx
@@ -1,10 +1,35 @@
+import { ArrowLeftCircleIcon, ArrowRightCircleIcon } from "@heroicons/react/24/solid";
 import PageContainer from "../../ui_components/PageContainer";
 import PageTitle from "../../ui_components/PageTitle";
 import { PlayerDataProps } from "../players/components/Types";
+import { useState } from "react";
+import Terms2025 from "../data/Terms";
+
+const getCurrentTermAndWeek = (currentDate: Date): {term: number; week: number;} => {
+    for (let i = 0; i < Terms2025.length; i++) {
+        const term = Terms2025[i];
+        const nextTerm = Terms2025[i + 1];
+
+        if (currentDate >= term.date && (!nextTerm || currentDate < nextTerm.date)) {
+            const weekNumber = Math.floor(
+                (currentDate.getTime() - term.date.getTime()) / (7 * 24 * 60 * 60 * 1000)
+            ) + 1;
+            
+            if (weekNumber <= term.weeks) {
+                return { term: i + 1, week: weekNumber };
+            }
+        }
+    }
+    return {term: -1, week: -1}; // Outside of term dates
+};
 
 const GameResults = (props: PlayerDataProps) => {
     const { ageGroups, teams } = props;
-    console.log(teams);
+    const [currentDate, setCurrentDate] = useState(new Date(2025, 1, 9));
+    console.log(currentDate);
+    console.log(getCurrentTermAndWeek(currentDate)?.term);
+
+
     return (
         <PageContainer>
             <PageTitle text="Game Results" />
@@ -14,7 +39,11 @@ const GameResults = (props: PlayerDataProps) => {
                     <h2 className="text-lg">4/28 recorded</h2>
                 </div>
                 <div className='inline-block h-5/6 mt-24 min-h-[1em] w-0.5 self-stretch bg-neutral-100 '></div>
-                <div className="w-1/2 bg-red-200 pt-4">e</div>
+                <div className="w-1/2 pt-4">
+                    <div className="text-xl font-bold float-left flex gap-2">
+                        <p>Term {getCurrentTermAndWeek(currentDate).term}, Week {getCurrentTermAndWeek(currentDate)?.week}</p>
+                    </div>
+                </div>
             </div>
         </PageContainer>
     );
