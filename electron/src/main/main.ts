84cbdceb41bb3d0775da2e1bf8d9cb9a766c8fba
Jamie Cole <jamiejcole@icloud.com>
2025-01-15 14:34:48 +1100

    removed dev tools from package

--- electron/src/main/main.ts
+++ electron/src/main/main.ts
@@ -161,7 +161,6 @@ const createWindow = async () => {
     });
 
     mainWindow.loadURL(resolveHtmlPath('index.html'));
-    mainWindow.webContents.openDevTools();
 
     mainWindow.on('ready-to-show', () => {
         if (!mainWindow) {
