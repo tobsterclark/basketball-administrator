09b2d1020e2ea1c579f7ae6c5e5ef35837a47f2e
Jamie Cole <jamiejcole@icloud.com>
2025-01-16 22:09:30 +1100

    fixed popup

--- electron/src/renderer/pages/Dashboard.tsx
+++ electron/src/renderer/pages/Dashboard.tsx
@@ -43,7 +43,7 @@ const Dashboard = () => {
                         <div className="absolute w-full ">
                             <div className="overflow-x-auto gap-6 pt-4 pb-4 min-width-max pr-[200px]">
                                 <p className='font-bold text-lg'>Version {appVersion}</p>
-                                {!updateAvailable ? (
+                                {updateAvailable ? (
                                     <div className='pt-4'>
                                         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
                                             <div className="bg-white rounded-lg p-6 shadow-lg w-96 text-center">
