970d3ebc9785761d18bced7536cea3669872c8d2
Jamie Cole <jamiejcole@icloud.com>
2025-01-17 14:27:33 +1100

    added darwin query engine for functions prisma schema

--- electron/src/renderer/pages/roster/Roster.tsx
+++ electron/src/renderer/pages/roster/Roster.tsx
@@ -32,10 +32,14 @@ enum Location {
 }
 
 const toTitleCase = (str: any) => {
-    return str.toLowerCase().split(' ').map((word: any) => {
-      return (word.charAt(0).toUpperCase() + word.slice(1));
-    }).join(' ');
-}
+    return str
+        .toLowerCase()
+        .split(' ')
+        .map((word: any) => {
+            return word.charAt(0).toUpperCase() + word.slice(1);
+        })
+        .join(' ');
+};
 
 const locationToText = (location: Location) => {
     switch (location) {
@@ -44,13 +48,12 @@ const locationToText = (location: Location) => {
         case Location.BELROSE:
             return 'Belrose';
     }
-}
+};
 
 const formatTime = (date: Date) => {
     // format time in <HH:mm> AM/PM format
     return date.toLocaleTimeString([], { hour: 'numeric', minute: '2-digit' });
-}
-    
+};
 
 type Team = {
     id: string;
@@ -91,74 +94,74 @@ type Event = {
 
 const courtResources = [
     {
-        text: "Court 1",
+        text: 'Court 1',
         id: 1,
-        color: "#DBB1BC"
+        color: '#DBB1BC',
     },
     {
-        text: "Court 2",
+        text: 'Court 2',
         id: 2,
-        color: "#6B0F1A"
+        color: '#6B0F1A',
     },
     {
-        text: "Court 3",
+        text: 'Court 3',
         id: 3,
-        color: "#212D40"
-    }
+        color: '#212D40',
+    },
 ];
 
 const venueResources = [
     {
-        text: "St Ives",
+        text: 'St Ives',
         id: Location.ST_IVES,
-        color: "#64b5f6"
+        color: '#64b5f6',
     },
     {
-        text: "Belrose",
+        text: 'Belrose',
         id: Location.BELROSE,
-        color: "#22c55e"
-    }
+        color: '#22c55e',
+    },
 ];
 
 const ageGroupResources = [
     {
-        text: "Years 3/4",
-        id: "Years 3/4",
-        color: "#F7EBEC"
+        text: 'Years 3/4',
+        id: 'Years 3/4',
+        color: '#F7EBEC',
     },
     {
-        text: "Years 5/6",
-        id: "Years 5/6",
-        color: "#DDBDD5"
+        text: 'Years 5/6',
+        id: 'Years 5/6',
+        color: '#DDBDD5',
     },
     {
-        text: "Years 7/8",
-        id: "Years 7/8",
-        color: "#AC9FBB"
+        text: 'Years 7/8',
+        id: 'Years 7/8',
+        color: '#AC9FBB',
     },
     {
-        text: "Years 9/12",
-        id: "Years 9/12",
-        color: "#59656F"
-    }
-]
+        text: 'Years 9/12',
+        id: 'Years 9/12',
+        color: '#59656F',
+    },
+];
 
 const appointmentResources = [
     {
-        fieldName: "location",
-        title: "location",
-        instances: venueResources
+        fieldName: 'location',
+        title: 'location',
+        instances: venueResources,
     },
     {
-        fieldName: "court",
-        title: "court",
-        instances: courtResources
+        fieldName: 'court',
+        title: 'court',
+        instances: courtResources,
     },
     {
-        fieldName: "ageGroup",
-        title: "ageGroup",
-        instances: ageGroupResources
-    }
+        fieldName: 'ageGroup',
+        title: 'ageGroup',
+        instances: ageGroupResources,
+    },
 ];
 
 const downloadRunsheet = async (gameId: string) => {
@@ -169,7 +172,12 @@ const downloadRunsheet = async (gameId: string) => {
 
     const toastId = toast.loading('Downloading PDF...');
     try {
-        const result = await window.electron.ipcRenderer.invoke('SavePDF', { url, defaultFileName });
+        const result = await window.electron.ipcRenderer.invoke('SavePDF', {
+            url,
+            defaultFileName,
+        });
+        console.log('result:');
+        console.log(result);
         if (result.success) {
             toast.update(toastId, {
                 render: `PDF saved successfully at ${result.filePath}`,
@@ -189,7 +197,9 @@ const downloadRunsheet = async (gameId: string) => {
         console.error('Error saving PDF:');
         console.error(error);
         toast.update(toastId, {
-            render: `An error occurred while saving the ZIP: ${(error as Error).message}`,
+            render: `An error occurred while saving the ZIP: ${
+                (error as Error).message
+            }`,
             type: 'error',
             isLoading: false,
             autoClose: 3000,
@@ -198,15 +208,22 @@ const downloadRunsheet = async (gameId: string) => {
 };
 
 const downloadMultipleRunsheets = async (gameIds: string[]) => {
-    const devURL = `http://127.0.0.1:5001/runsheetcontrol/australia-southeast1/generaterunsheets?gameIds=[${gameIds.join(',')}]`;
-    const prodURL = `https://australia-southeast1-runsheetcontrol.cloudfunctions.net/generaterunsheets?gameIds=[${gameIds.join(',')}]`;
+    const devURL = `http://127.0.0.1:5001/runsheetcontrol/australia-southeast1/generaterunsheets?gameIds=[${gameIds.join(
+        ',',
+    )}]`;
+    const prodURL = `https://australia-southeast1-runsheetcontrol.cloudfunctions.net/generaterunsheets?gameIds=[${gameIds.join(
+        ',',
+    )}]`;
     const url = process.env.NODE_ENV === 'development' ? devURL : prodURL;
 
     const defaultFileName = `scoresheets.zip`;
 
     const toastId = toast.loading('Downloading ZIP...');
     try {
-        const result = await window.electron.ipcRenderer.invoke('SaveZIP', { url, defaultFileName });
+        const result = await window.electron.ipcRenderer.invoke('SaveZIP', {
+            url,
+            defaultFileName,
+        });
         if (result.success) {
             toast.update(toastId, {
                 render: `ZIP saved successfully at ${result.filePath}`,
@@ -226,7 +243,9 @@ const downloadMultipleRunsheets = async (gameIds: string[]) => {
         console.error('Error saving ZIP:');
         console.error(error);
         toast.update(toastId, {
-            render: `An error occurred while saving the ZIP: ${(error as Error).message}`,
+            render: `An error occurred while saving the ZIP: ${
+                (error as Error).message
+            }`,
             type: 'error',
             isLoading: false,
             autoClose: 3000,
@@ -234,12 +253,35 @@ const downloadMultipleRunsheets = async (gameIds: string[]) => {
     }
 };
 
-const CustomAppointment = ({ children, data, ...restProps }: { children: React.ReactNode, data: any }) => (
-    <Appointments.Appointment {...restProps} className="!bg-transparent !cursor-default" data={data}  draggable={false} resources={[]}>
-        <div className={`text-white px-1 py-1 ${data.location === Location.ST_IVES ? 'bg-blue-500' : 'bg-green-500'}`}>
-            <div className='font-bold'>{data.title}</div>
-            <div>{formatTime(data.startDate)} - {formatTime(data.endDate)}</div>
-            <div>{locationToText(data.location)} - Court {data.court}</div>
+const CustomAppointment = ({
+    children,
+    data,
+    ...restProps
+}: {
+    children: React.ReactNode;
+    data: any;
+}) => (
+    <Appointments.Appointment
+        {...restProps}
+        className="!bg-transparent !cursor-default"
+        data={data}
+        draggable={false}
+        resources={[]}
+    >
+        <div
+            className={`text-white px-1 py-1 ${
+                data.location === Location.ST_IVES
+                    ? 'bg-blue-500'
+                    : 'bg-green-500'
+            }`}
+        >
+            <div className="font-bold">{data.title}</div>
+            <div>
+                {formatTime(data.startDate)} - {formatTime(data.endDate)}
+            </div>
+            <div>
+                {locationToText(data.location)} - Court {data.court}
+            </div>
             <div>{data.ageGroup}</div>
         </div>
     </Appointments.Appointment>
@@ -250,11 +292,16 @@ const getCurrentTermAndWeek = (currentDate: Date) => {
         const term = Terms2025[i];
         const nextTerm = Terms2025[i + 1];
 
-        if (currentDate >= term.date && (!nextTerm || currentDate < nextTerm.date)) {
-            const weekNumber = Math.floor(
-                (currentDate.getTime() - term.date.getTime()) / (7 * 24 * 60 * 60 * 1000)
-            ) + 1;
-            
+        if (
+            currentDate >= term.date &&
+            (!nextTerm || currentDate < nextTerm.date)
+        ) {
+            const weekNumber =
+                Math.floor(
+                    (currentDate.getTime() - term.date.getTime()) /
+                        (7 * 24 * 60 * 60 * 1000),
+                ) + 1;
+
             if (weekNumber <= term.weeks) {
                 return { term: i + 1, week: weekNumber };
             }
@@ -263,8 +310,17 @@ const getCurrentTermAndWeek = (currentDate: Date) => {
     return null; // Outside of term dates
 };
 
-const CustomToolbar = ({ currentDate, ...restProps }: { currentDate: Date, children?: React.ReactNode }) => {
-    const termAndWeek = useMemo(() => getCurrentTermAndWeek(currentDate), [currentDate]);
+const CustomToolbar = ({
+    currentDate,
+    ...restProps
+}: {
+    currentDate: Date;
+    children?: React.ReactNode;
+}) => {
+    const termAndWeek = useMemo(
+        () => getCurrentTermAndWeek(currentDate),
+        [currentDate],
+    );
 
     return (
         <Toolbar.Root {...restProps}>
@@ -287,8 +343,22 @@ const CustomToolbar = ({ currentDate, ...restProps }: { currentDate: Date, child
     );
 };
 
-const CustomTooltipHeader = ({ appointmentData, ...restProps }: { appointmentData?: any, [key: string]: any }) => (
-    <AppointmentTooltip.Header {...restProps} appointmentData={appointmentData} showOpenButton showCloseButton showDeleteButton commandButtonIds={['a']} commandButtonComponent={() => null}>
+const CustomTooltipHeader = ({
+    appointmentData,
+    ...restProps
+}: {
+    appointmentData?: any;
+    [key: string]: any;
+}) => (
+    <AppointmentTooltip.Header
+        {...restProps}
+        appointmentData={appointmentData}
+        showOpenButton
+        showCloseButton
+        showDeleteButton
+        commandButtonIds={['a']}
+        commandButtonComponent={() => null}
+    >
         <Button
             variant="contained"
             color="primary"
@@ -313,11 +383,13 @@ const Roster = (props: PlayerDataProps) => {
     };
 
     const getGameIdsForDate = (date: Date): string[] => {
-        return allGames.filter((game) => {
-            const gameDate = new Date(game.timeslot.date);
-            return gameDate.toDateString() === date.toDateString();
-        }).map((game) => game.id);
-    }
+        return allGames
+            .filter((game) => {
+                const gameDate = new Date(game.timeslot.date);
+                return gameDate.toDateString() === date.toDateString();
+            })
+            .map((game) => game.id);
+    };
 
     const transformGamesToEvents = (games: Game[]) => {
         const events: Event[] = games.map((game: Game) => {
@@ -325,7 +397,9 @@ const Roster = (props: PlayerDataProps) => {
             const endDate = new Date(startDate);
             endDate.setHours(startDate.getHours() + 1);
             const ageGroupIdStr: string = game.lightTeam.ageGroupId;
-            const ageGroupName = ageGroups.find((ageGroup) => ageGroup.id === ageGroupIdStr)?.displayName;
+            const ageGroupName = ageGroups.find(
+                (ageGroup) => ageGroup.id === ageGroupIdStr,
+            )?.displayName;
             return {
                 title: `${game.lightTeam.name} vs ${game.darkTeam.name}`,
                 startDate,
@@ -336,7 +410,7 @@ const Roster = (props: PlayerDataProps) => {
                 ageGroup: toTitleCase(ageGroupName),
             };
         });
-    
+
         // Sort events by location first (so st ives is first, belrose is second), then sort by court number
         events.sort((a, b) => {
             if (a.location < b.location) return 1;
@@ -345,7 +419,7 @@ const Roster = (props: PlayerDataProps) => {
             if (a.court > b.court) return 1;
             return 0;
         });
-    
+
         setAllEvents(events);
     };
 
@@ -376,32 +450,69 @@ const Roster = (props: PlayerDataProps) => {
     return (
         <PageContainer>
             <PageTitle text="Roster" />
-            <div className='flex gap-6 h-full'>
-                <div className='w-3/5'>
-                    <Scheduler data={allEvents}>                
-                        <ViewState defaultCurrentDate={new Date(2025, 1, 9)} currentDate={currentDate} onCurrentDateChange={setCurrentDate}  />
-                        <WeekView excludedDays={[1, 2, 3, 4, 5, 6]} startDayHour={8} endDayHour={16} />
-                        <Toolbar rootComponent={(props) => (
-                            <CustomToolbar currentDate={currentDate} {...props} />
-                        )} />
+            <div className="flex gap-6 h-full">
+                <div className="w-3/5">
+                    <Scheduler data={allEvents}>
+                        <ViewState
+                            defaultCurrentDate={new Date(2025, 1, 9)}
+                            currentDate={currentDate}
+                            onCurrentDateChange={setCurrentDate}
+                        />
+                        <WeekView
+                            excludedDays={[1, 2, 3, 4, 5, 6]}
+                            startDayHour={8}
+                            endDayHour={16}
+                        />
+                        <Toolbar
+                            rootComponent={(props) => (
+                                <CustomToolbar
+                                    currentDate={currentDate}
+                                    {...props}
+                                />
+                            )}
+                        />
                         <DateNavigator />
-                        <Appointments appointmentComponent={CustomAppointment}/>
-                        <AppointmentTooltip showOpenButton headerComponent={CustomTooltipHeader} />
+                        <Appointments
+                            appointmentComponent={CustomAppointment}
+                        />
+                        <AppointmentTooltip
+                            showOpenButton
+                            headerComponent={CustomTooltipHeader}
+                        />
                         <AppointmentForm />
-                        <Resources data={appointmentResources} mainResourceName='location' />
+                        <Resources
+                            data={appointmentResources}
+                            mainResourceName="location"
+                        />
                     </Scheduler>
                 </div>
-                <div className='inline-block h-5/6 mt-24 min-h-[1em] w-0.5 self-stretch bg-neutral-100 '></div>
-                <div className='flex-col pl-8 justify-center w-2/5 pt-16'>
-                    <h2 className='text-lg text-slate-800 font-bold underline underline-offset-4 decoration-4 decoration-dustyBlue'>
-                        {getCurrentTermAndWeek(currentDate)?.term !== undefined ? 
-                            (`Manage Games for Term ${getCurrentTermAndWeek(currentDate)?.term}, Week ${getCurrentTermAndWeek(currentDate)?.week}`) : 
-                            ('Manage Games')}
+                <div className="inline-block h-5/6 mt-24 min-h-[1em] w-0.5 self-stretch bg-neutral-100 "></div>
+                <div className="flex-col pl-8 justify-center w-2/5 pt-16">
+                    <h2 className="text-lg text-slate-800 font-bold underline underline-offset-4 decoration-4 decoration-dustyBlue">
+                        {getCurrentTermAndWeek(currentDate)?.term !== undefined
+                            ? `Manage Games for Term ${getCurrentTermAndWeek(
+                                  currentDate,
+                              )?.term}, Week ${getCurrentTermAndWeek(
+                                  currentDate,
+                              )?.week}`
+                            : 'Manage Games'}
                     </h2>
-                    <p className='pt-4 pb-8'>{getGameCountForDate(currentDate)} games across all venues and courts.</p>
-                    <Button variant="contained" size="medium" className='flex items-center' onClick={async () => await downloadMultipleRunsheets(getGameIdsForDate(currentDate))}>
-                        Download Scoresheets 
-                        <ArrowDownOnSquareStackIcon className='h-6 ml-2' />
+                    <p className="pt-4 pb-8">
+                        {getGameCountForDate(currentDate)} games across all
+                        venues and courts.
+                    </p>
+                    <Button
+                        variant="contained"
+                        size="medium"
+                        className="flex items-center"
+                        onClick={async () =>
+                            await downloadMultipleRunsheets(
+                                getGameIdsForDate(currentDate),
+                            )
+                        }
+                    >
+                        Download Scoresheets
+                        <ArrowDownOnSquareStackIcon className="h-6 ml-2" />
                     </Button>
                 </div>
             </div>
